c1faa231b256063400b827dd5c8399ed1b1cd0a4
Wahid Rahim <wahidrahim@gmail.com>
2021-07-26 16:53:32 -0400

    refactor(Flex): move styled components out
    split types to spearate file

--- src/components/Flex/Flex.tsx
+++ src/components/Flex/Flex.tsx
@@ -1,16 +1,7 @@
 import { FC } from 'react';
-import styled from 'styled-components';
 
-import { Box, BoxProps, BoxVariant } from '../Box';
-import { Justify, Align, propValueMap } from '../../theme/types';
-
-export type FlexVariant = BoxVariant;
-
-export type FlexProps = BoxProps & {
-  justify?: Justify;
-  align?: Align;
-  dir?: 'row' | 'column';
-};
+import { FlexProps } from './types';
+import { Component } from './styles';
 
 export const Flex: FC<FlexProps> = ({
   variant,
@@ -19,12 +10,13 @@ export const Flex: FC<FlexProps> = ({
   dir,
   ...props
 }) => {
-  const Component = styled(Box)(({ theme }) => ({
-    ...(theme.FLEX || {}),
-    display: 'flex',
-    ...(justify && { justifyContent: propValueMap[justify] || justify }),
-    ...(align && { alignItems: propValueMap[align] || align }),
-    ...(dir && { flexDirection: dir }),
-  }));
-  return <Component variant={variant} {...props} />;
+  return (
+    <Component
+      justify={justify}
+      align={align}
+      dir={dir}
+      variant={variant}
+      {...props}
+    />
+  );
 };
---
+++ src/components/Flex/types.ts
@@ -0,0 +1,10 @@
+import { Justify, Align } from '../../theme/types';
+import { BoxVariant, BoxProps } from '../Box';
+
+export type FlexVariant = BoxVariant;
+
+export type FlexProps = BoxProps & {
+  justify?: Justify;
+  align?: Align;
+  dir?: 'row' | 'column';
+};
