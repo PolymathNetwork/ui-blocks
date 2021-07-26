e813a63e95bfb1aed8953aa460df3c85af46bb49
Wahid Rahim <wahidrahim@gmail.com>
2021-07-26 11:42:33 -0400

    refactor(Heading): move styled components out

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -12,18 +12,28 @@ export type HeadingProps = {
   altColor?: string;
 };
 
+const Component = styled.h1<HeadingProps & any>(
+  ({ theme, color, altColor, margin, variant }) => ({
+    ...(theme.TYPOGRAPHY[variant] || {}),
+    margin: getMargin({ theme, margin }),
+    ...(color ? { color: theme.COLOR[color] } : {}),
+    ...(altColor ? { span: { color: theme.COLOR[altColor] } } : {}),
+  })
+);
+
 export const Heading: FC<HeadingProps> = ({
   variant,
   color,
   altColor,
   margin,
   ...props
-}) => {
-  const Component = styled[variant](({ theme }) => ({
-    ...(theme.TYPOGRAPHY[variant] || {}),
-    margin: getMargin({ theme, margin }),
-    ...(color ? { color: theme.COLOR[color] } : {}),
-    ...(altColor ? { span: { color: theme.COLOR[altColor] } } : {}),
-  }));
-  return <Component {...props} />;
-};
+}) => (
+  <Component
+    as={variant}
+    variant={variant}
+    color={color}
+    altColor={altColor}
+    margin={margin}
+    {...props}
+  />
+);
