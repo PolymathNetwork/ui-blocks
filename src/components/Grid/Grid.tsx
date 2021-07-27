3cf85081f91297f20c2805ccc154b40d58c5b33e
Wahid Rahim <wahidrahim@gmail.com>
2021-07-27 15:29:13 -0400

    refactor(Grid): split into multiple files

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,47 +1,10 @@
 import { FC } from 'react';
-import styled from 'styled-components';
+import { Box } from '../Box';
+import { Flex } from '../Flex';
+import { GridItemProps, GridProps } from './types';
+import { Component, ItemWrapper } from './styles';
 
-import { Box, BoxProps, BoxVariant } from '../Box';
-import { Flex, FlexProps, FlexVariant } from '../Flex';
-import { Justify, Align, propValueMap } from '../../theme/types';
-
-export type GridVariant = BoxVariant;
-
-export type GridProps = BoxProps & {
-  justify?: Justify;
-  align?: Align;
-  cols?: string;
-  rows?: string;
-  gap?: string;
-};
-
-const Component = styled(Box)<GridProps>(
-  ({ theme, justify, align, cols, rows, gap }) => ({
-    ...(theme.GRID || {}),
-    display: 'grid',
-    ...(justify && { justifyContent: propValueMap[justify] || justify }),
-    ...(align && { alignItems: propValueMap[align] || align }),
-    ...(cols && { gridTemplateColumns: cols }),
-    ...(rows && { gridTemplateRows: rows }),
-    ...(gap && { gridGap: gap }),
-  })
-);
-
-const GridComponent: FC<GridProps> = ({ ...props }) => {
-  return <Component {...props} />;
-};
-
-type GridItemProps = BoxProps &
-  FlexProps &
-  GridProps & {
-    as: 'Box' | 'Flex' | 'Grid';
-    variant: BoxVariant | FlexVariant | GridVariant;
-    area: string;
-  };
-
-const ItemWrapper = styled.div<GridItemProps & any>`
-  grid-area: ${({ area }) => area};
-`;
+const GridComponent: FC<GridProps> = ({ ...props }) => <Component {...props} />;
 
 const Item: FC<GridItemProps> = ({ as, ...itemProps }) => {
   const TargetComponent = {
