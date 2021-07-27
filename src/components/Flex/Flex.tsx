d4bfaf74dea9b109b662170ff9dd54d1f74de8e2
Wahid Rahim <wahidrahim@gmail.com>
2021-07-26 22:39:32 -0400

    refactor(Flex): remove redundant prop assignments

--- src/components/Flex/Flex.tsx
+++ src/components/Flex/Flex.tsx
@@ -3,20 +3,6 @@ import { FC } from 'react';
 import { FlexProps } from './types';
 import { Component } from './styles';
 
-export const Flex: FC<FlexProps> = ({
-  variant,
-  justify,
-  align,
-  dir,
-  ...props
-}) => {
-  return (
-    <Component
-      justify={justify}
-      align={align}
-      dir={dir}
-      variant={variant}
-      {...props}
-    />
-  );
+export const Flex: FC<FlexProps> = (props) => {
+  return <Component {...props} />;
 };
