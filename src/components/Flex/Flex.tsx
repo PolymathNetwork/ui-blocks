7062e0e166a4e3aaaa98f3a9b80bad027ee85f0b
Wahid Rahim <wahidrahim@gmail.com>
2021-07-28 11:28:06 -0400

    refactor(Flex): put types and styles in main file

--- src/components/Flex/Flex.tsx
+++ src/components/Flex/Flex.tsx
@@ -1,7 +1,24 @@
 import { FC } from 'react';
+import styled from 'styled-components';
 
-import { FlexProps } from './types';
-import { Component } from './styles';
+import { Justify, Align, propValueMap } from '../../theme/types';
+import { Box, BoxVariant, BoxProps } from '../Box';
+
+export type FlexVariant = BoxVariant;
+
+export type FlexProps = BoxProps & {
+  justify?: Justify;
+  align?: Align;
+  dir?: 'row' | 'column';
+};
+
+const Component = styled(Box)<FlexProps>(({ justify, align, dir, theme }) => ({
+  ...(theme.FLEX || {}),
+  display: 'flex',
+  ...(justify && { justifyContent: propValueMap[justify] || justify }),
+  ...(align && { alignItems: propValueMap[align] || align }),
+  ...(dir && { flexDirection: dir }),
+}));
 
 export const Flex: FC<FlexProps> = (props) => {
   return <Component {...props} />;
