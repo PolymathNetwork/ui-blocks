204ffa9ba8e1451c23294a6ccd0d4f7b6f28c791
Wahid Rahim <wahidrahim@gmail.com>
2021-07-28 11:09:35 -0400

    refactor(Badge): put types and styles in main file

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,7 +1,36 @@
 import { FC } from 'react';
+import styled from 'styled-components';
 
-import { BadgeProps } from './types';
-import { Component } from './styles';
+import { Display, CSSPropertiesExtended } from '../../theme/types';
+import { getMargin } from '../../theme/utils';
+
+export type BadgeVariant = 'basic' | 'success' | 'warning' | 'danger';
+
+export type BadgeProps = {
+  variant: BadgeVariant;
+  margin?: string;
+  display?: Display;
+  size?: 's' | 'm';
+};
+
+const sizeMap: Record<string, CSSPropertiesExtended> = {
+  s: {
+    padding: '2px 8px',
+    fontSize: '10px !important',
+  },
+  m: {
+    padding: '5px 16px',
+  },
+};
+
+export const Component = styled.span<BadgeProps>(
+  ({ theme, variant, display, margin, size }) => ({
+    ...(theme.BADGE[variant] || {}),
+    display,
+    margin: getMargin({ theme, margin }),
+    ...(size ? sizeMap[size] : {}),
+  }),
+);
 
 export const Badge: FC<BadgeProps> = ({
   variant,
