051d5a868e95b247e8d156c1b519db68833e45c8
Wahid Rahim <wahidrahim@gmail.com>
2021-07-28 12:56:24 -0400

    refactor(Icon): put types and styles in main file

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,30 +1,57 @@
 import { FC } from 'react';
+import styled from 'styled-components';
 
-import { IconProps } from './types';
-import { Component } from './styles';
+import { IconType } from '../../theme/types';
+import { getMargin } from '../../theme/utils';
+
+export type IconVariant = 'basic' | 'circle';
+
+export type IconProps = {
+  variant: IconVariant;
+  margin?: string;
+  icon: IconType;
+  size: string;
+  color?: string;
+  bg?: string;
+  scale?: number;
+  rotate?: string;
+};
+
+const Component = styled.span<Omit<IconProps, 'icon'>>(
+  ({ variant, margin, size, bg, scale, rotate, color, theme }) => ({
+    ...(theme.ICON[variant] || {}),
+    display: 'inline-block',
+    margin: getMargin({ theme, margin }),
+    padding: 0,
+    width: size,
+    height: size,
+    ...(bg && theme.COLOR[bg] && { backgroundColor: theme.COLOR[bg] }),
+    svg: {
+      display: 'block',
+      width: '100%',
+      height: '100%',
+      ...(scale && { padding: `${(1 - scale) * 100}%` }),
+      ...(rotate && { transform: `rotateZ(${rotate})` }),
+    },
+    'svg > *': {
+      ...theme.ICON[variant]['svg > *'],
+      ...(color || theme.ICON[variant].fill
+        ? {
+            fill: color ? theme.COLOR[color] : theme.ICON[variant].fill,
+          }
+        : {}),
+    },
+  }),
+);
 
 export const Icon: FC<IconProps> = ({
   variant,
   icon: SvgIcon,
-  size,
-  color,
-  bg,
-  rotate,
   scale = variant === 'circle' ? 0.9 : undefined,
-  margin,
   ...props
 }) => {
   return (
-    <Component
-      variant={variant}
-      size={size}
-      color={color}
-      bg={bg}
-      rotate={rotate}
-      scale={scale}
-      margin={margin}
-      {...props}
-    >
+    <Component variant={variant} scale={scale} {...props}>
       <SvgIcon />
     </Component>
   );
