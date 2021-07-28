efa314a9e2f8b1a685e420aaeefd1cc7dedb7f3d
Wahid Rahim <wahidrahim@gmail.com>
2021-07-28 12:53:56 -0400

    refactor(Heading): put types and styles in main file

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,21 +1,24 @@
 import { FC } from 'react';
+import styled from 'styled-components';
 
-import { HeadingProps } from './types';
-import { Component } from './styles';
+import { getMargin } from '../../theme/utils';
 
-export const Heading: FC<HeadingProps> = ({
-  variant,
-  color,
-  altColor,
-  margin,
-  ...props
-}) => (
-  <Component
-    as={variant}
-    variant={variant}
-    color={color}
-    altColor={altColor}
-    margin={margin}
-    {...props}
-  />
+export type HeadingVariant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
+export type HeadingProps = {
+  variant: HeadingVariant;
+  margin?: string;
+  color?: string;
+  altColor?: string;
+};
+
+const Component = styled.h1<HeadingProps>(
+  ({ theme, color, altColor, margin, variant }) => ({
+    ...(theme.TYPOGRAPHY[variant] || {}),
+    margin: getMargin({ theme, margin }),
+    ...(color ? { color: theme.COLOR[color] } : {}),
+    ...(altColor ? { span: { color: theme.COLOR[altColor] } } : {}),
+  }),
 );
+
+export const Heading: FC<HeadingProps> = (props) => <Component {...props} />;
--- src/components/Heading/types.ts
+++
@@ -1,8 +0,0 @@
-export type HeadingVariant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
-
-export type HeadingProps = {
-  variant: HeadingVariant;
-  margin?: string;
-  color?: string;
-  altColor?: string;
-};
