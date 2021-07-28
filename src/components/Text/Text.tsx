1b5b8dc6bf97181d1e9272ba3e976896745f4afe
Wahid Rahim <wahidrahim@gmail.com>
2021-07-28 13:06:10 -0400

    refactor(Text): put types and styles in main file

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,25 +1,31 @@
 import { FC } from 'react';
+import styled from 'styled-components';
 
-import { Component } from './styles';
-import { TextProps } from './types';
+import { TextFormat } from '../../theme/types';
+import { getMargin } from '../../theme/utils';
 
-export const Text: FC<TextProps> = ({
-  variant,
-  format,
-  color,
-  altColor,
-  display,
-  margin,
-  ...props
-}) => (
-  <Component
-    as={variant}
-    variant={variant}
-    format={format}
-    color={color}
-    altColor={altColor}
-    display={display}
-    margin={margin}
-    {...props}
-  />
+export type TextVariant = 'p' | 'span' | 'label';
+
+export type TextProps = {
+  variant: TextVariant;
+  margin?: string;
+  format?: TextFormat;
+  color?: string;
+  altColor?: string;
+  display?: 'block' | 'inline-block' | 'inline' | 'none';
+};
+
+export const Component = styled.span<TextProps>(
+  ({ variant, format, margin, color, altColor, display, theme }) => ({
+    ...(theme.TEXT[variant] || {}),
+    ...(format && (theme.TYPOGRAPHY[format] || {})),
+    margin: getMargin({ theme, margin }),
+    ...(color && { color: theme.COLOR[color] }),
+    ...(altColor ? { span: { color: theme.COLOR[altColor] } } : {}),
+    ...(display && { display }),
+  }),
+);
+
+export const Text: FC<TextProps> = ({ variant, ...props }) => (
+  <Component as={variant} variant={variant} {...props} />
 );
