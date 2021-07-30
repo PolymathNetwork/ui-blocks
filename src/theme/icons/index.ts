19adb3a720d519f4f181862d8753d1e4c3766972
honarpour <amir@honarpour.com>
2021-07-30 10:15:01 -0400

    chore(icons): dynamic export

--- src/theme/icons/index.ts
+++ src/theme/icons/index.ts
@@ -1,1 +1,26 @@
-export * as polyIcons from './tsx';
+import { ComponentType } from 'react';
+import path from 'path';
+
+const reqSvgs = require.context('./svg', true, /\.svg$/);
+let reqTsxs: any = [];
+try {
+  reqTsxs = require.context('./tsx', true, /\.tsx?$/);
+} catch (e) {} // eslint-disable-line
+const req = reqTsxs.length ? reqTsxs : reqSvgs;
+
+const polyIcons: Record<string, ComponentType> = {};
+
+const toExportName = (filename: string) =>
+  (/^\d/.test(filename) ? `Svg${filename}` : filename)
+    .toLowerCase()
+    .split('-')
+    .map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+    .join('');
+
+req.keys().forEach((file: any) => {
+  const basename = path.basename(file, path.extname(file));
+  const exportName = toExportName(basename);
+  polyIcons[exportName] = req(file).default;
+});
+
+export { polyIcons };
--- src/theme/icons/template-index.js
+++ src/theme/icons/template-index.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
-function indexTemplate(files) {
+const indexTemplate = (files) => {
   const exportEntries = files.map((file) => {
     const basename = path.basename(file, path.extname(file));
     const exportName = /^\d/.test(basename) ? `Svg${basename}` : basename;
     return `export { default as ${exportName} } from './${basename}';`;
   });
   return `${exportEntries.join('\n')}\n`;
-}
+};
 
 module.exports = indexTemplate;
