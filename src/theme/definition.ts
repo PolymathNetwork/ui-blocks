a93db51e5a9f9c025bece80f9bfd70fcdef3988f
Mehdi Raash <leasksin@gmail.com>
2021-08-25 23:24:42 -0400

    chore: linting update

--- src/theme/definition.ts
+++ src/theme/definition.ts
@@ -489,11 +489,11 @@ export const SELECT: any = {
     borderColor: COLOR.gray3,
     ...(state.isFocused
       ? {
-        borderColor: COLOR.brandMain,
-        '&:hover': {
           borderColor: COLOR.brandMain,
-        },
-      }
+          '&:hover': {
+            borderColor: COLOR.brandMain,
+          },
+        }
       : {}),
     cursor: 'pointer',
   }),
@@ -515,9 +515,9 @@ export const SELECT: any = {
     color: COLOR.gray1,
     ...(state.isSelected
       ? {
-        color: COLOR.gray1,
-        backgroundColor: COLOR.gray5,
-      }
+          color: COLOR.gray1,
+          backgroundColor: COLOR.gray5,
+        }
       : {}),
     '&:hover': {
       color: COLOR.gray1,
