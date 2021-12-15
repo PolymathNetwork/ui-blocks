2beffa0b3279c3d1cca5a06bc9a9e06cda22f3ee
Wahid Rahim <wahidrahim@gmail.com>
2021-12-15 16:34:31 -0500

    refactor: icons, to provide types, and display all

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -1,9 +1,11 @@
-import { ComponentProps } from 'react';
+import React, { ComponentProps } from 'react';
 import { Story } from '@storybook/react';
 
+import { PolyIcons } from 'theme/icons';
 import { Icon } from './Icon';
 import { polyIcons } from '../../theme';
-// import { Flex } from '../Flex';
+import { Text } from '../Text';
+import { Grid } from '../Grid';
 
 export default {
   title: 'Polyblocks/Icon',
@@ -28,14 +30,20 @@ Circle.args = {
   size: '32px',
 };
 
-// export const All = () =>
-//   Object.keys(polyIcons).map((icon) => {
-//     const TargetIcon = polyIcons[icon];
-//     return (
-//       <Flex variant="raw" key={icon} margin="0 0 l 0" align="center">
-//         {icon}:
-//         <Icon variant="basic" icon={TargetIcon} size="48px" margin="0 m" />
-//         <Icon variant="circle" icon={TargetIcon} size="48px" />
-//       </Flex>
-//     );
-//   });
+export const All = () => (
+  <Grid variant="raw" cols="repeat(3, min-content)" gap="s">
+    {Object.keys(polyIcons).map((icon) => {
+      const TargetIcon = polyIcons[icon as keyof PolyIcons];
+
+      return (
+        <React.Fragment key={icon}>
+          <Text variant="b1" as="span" align="right">
+            {icon}:
+          </Text>
+          <Icon variant="basic" icon={TargetIcon} size="48px" margin="0 m" />
+          <Icon variant="circle" icon={TargetIcon} size="48px" />
+        </React.Fragment>
+      );
+    })}
+  </Grid>
+);
