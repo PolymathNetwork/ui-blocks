f772339fec970445030058456d7d23b630fd99b2
Wahid Rahim <wahidrahim@gmail.com>
2022-10-25 21:42:03 -0400

    refactor: update Chips component

--- src/components/Chips/Chips.tsx
+++ src/components/Chips/Chips.tsx
@@ -1,25 +1,26 @@
-import { FC } from 'react';
+import { PropsWithChildren } from 'react';
 import styled from 'styled-components';
 import { Flex } from '../Flex';
 
 export type ChipsVariant = 'default';
 
-export type ChipsProps = {
+export type ChipsProps = PropsWithChildren<{
   text: string;
   number: number;
   variant: 'default';
-};
+}>;
 
 const Component = styled(Flex)<any>(({ theme, variant }) => ({
   ...(theme.CHIPS[variant] || {}),
 }));
 
-export const Chips: FC<ChipsProps> = (props) => {
-  const { text, number } = props;
+export function Chips(chipsProps: ChipsProps) {
+  const { text, number, ...props } = chipsProps;
+
   return (
     <Component {...props}>
       {text}
       <div className="number">{number}</div>
     </Component>
   );
-};
+}
