3d7e2d5c0d648403308093c931e4c63afccea55e
Wahid Rahim <wahidrahim@gmail.com>
2022-10-26 14:23:00 -0400

    refactor: update Table component

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,10 +1,8 @@
-import { FC } from 'react';
-
 import { TableBasic } from './TableBasic';
 import { TableSubset } from './TableSubset';
 import { TableProps } from './TableTypes';
 
-export const Table: FC<TableProps> = (props) => {
+export const Table = (props: TableProps) => {
   const { fetchData } = props;
   const TableComponent = fetchData ? TableSubset : TableBasic;
   return <TableComponent {...props} />;
