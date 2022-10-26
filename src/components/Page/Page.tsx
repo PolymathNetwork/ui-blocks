35bcb360c86c5ee0362e999d5869a7ab657ca156
Wahid Rahim <wahidrahim@gmail.com>
2022-10-26 14:05:52 -0400

    refactor: update Page component

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,15 +1,15 @@
-import { FC } from 'react';
+import { PropsWithChildren } from 'react';
 import styled from 'styled-components';
 
 import { getMargin } from '../../theme/utils';
 
-export type PageProps = {
+export type PageProps = PropsWithChildren<{
   margin?: string;
-};
+}>;
 
 const Component = styled.div<PageProps>(({ theme, margin }) => ({
   ...(theme.PAGE || {}),
   margin: getMargin({ theme, margin }),
 }));
 
-export const Page: FC<PageProps> = (props) => <Component {...props} />;
+export const Page = (props: PageProps) => <Component {...props} />;
